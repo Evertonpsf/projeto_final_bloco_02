ff91fdb5fc01e4031cda2f66dc22938c0edc153f
Everton Sales <pinheiroe49@gmail.com>
2024-08-09 09:16:02 -0300

    01 Configurando Projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+    // estamos fazendo a conexão com o banco de dados.
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'db_farmacia',
+    entities: [],
+    synchronize: true,
+   // logging: true,
+   // bigNumberStrings: false,
+
+  }),
+ // CategoriaModules,
+ // ProdutoModules,
+],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
